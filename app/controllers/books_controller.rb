22006cea048a70f4099926231291dbc6c482c6b0
Robin Pahlman <robin.pahlman@gmail.com>
2017-12-11 21:20:52 -0500

    add book new, create, show actions, form and views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,30 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end 
+
+  def new
+    @book = Book.new 
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end 
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end 
+  end 
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :author, :year_published, :description)
+    end 
+
 end
