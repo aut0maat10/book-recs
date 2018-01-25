c435584d351ad1e96b2097111504fe686ef9d6b2
Robin Pahlman <robin.pahlman@gmail.com>
2018-01-25 14:55:36 -0500

    grab input data from form with jquery

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -9,6 +9,16 @@ $(function () {
   
   $('#new_review').submit(function(e) {
     alert('wy u clik dis');
+    url = this.action
+
+    data = {
+      'authenticity_token': $("input[name='authenticity_token']").val(),
+      'review': {
+        'rating': $("input[name='review[rating]'").val(),
+        'comment': $('#review_comment').val() 
+      }
+    }
+    console.log(data)
     e.preventDefault();  
   })
 })
