7b03acab200c4792c3835bc25904c4f8d02c0883
Robin Pahlman <robin.pahlman@gmail.com>
2018-01-25 14:18:32 -0500

    move review form to books show page, hijack submit button

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -5,5 +5,10 @@ $(function () {
       $('div.reviews').html(response);
     })
     e.preventDefault();
-  }) 
+  })
+  
+  $('#new_review').submit(function(e) {
+    alert('wy u clik dis');
+    e.preventDefault();  
+  })
 })
