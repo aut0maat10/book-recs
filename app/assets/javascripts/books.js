ded0374c68c7acae83c515e6ded34f584deb32ce
Robin Pahlman <robin.pahlman@gmail.com>
2018-02-02 15:14:36 -0500

    working prototype complete

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,6 +1,8 @@
 $(function() {
   compileIndexBookTemplate();
+  // compileShowBookTemplate();
   indexBooks();
+  // showBook(); 
 });
 
 class Book {
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,77 +1,52 @@
-function Review(attributes){
-  this.id = attributes.id;
-  this.rating = attributes.rating;
-  this.comment = attributes.comment; 
-}
+// function Review(attributes){
+//   this.id = attributes.id;
+//   this.rating = attributes.rating;
+//   this.comment = attributes.comment; 
+// }
 
-Review.success = function(json) {
-  var review = new Review(json);
-  var reviewHTML = review.renderHTML(); 
-  $("#new_review").trigger("reset");
-  $('div.reviews').append(reviewHTML)
-  // location.reload();
-  
-  // $('.review-rating').raty({ score: this.rating });
-  console.log(review)
-
-  
-  //$('a.show_reviews').trigger("click");
-  // $('input[name="review[rating]"]').val("")
-  //$("#new_review").trigger("reset");
-}
-Review.error = function (response) {
-  console.log("Yu broke it?", response)
-}
-
-
-Review.ready = (function () {
-  Review.templateSource = $('#review-template').html();
-  Review.template = Handlebars.compile(Review.templateSource);
-})
-
-Review.prototype.renderHTML = function(){
-  return Review.template(this)
-}
-
-// $(function () {
-//   $('a.show_reviews').on('click', function(e) {
-//     $(this).hide();
-//     $.get(this.href).success(function(response){
-//       $('div.reviews').html(response);
-//     })
-//     e.preventDefault();
-//   })
-  
-  // form for reviews
-
-  // Review.formSubmit = function (e) {
-  //   e.preventDefault()
-  //   var $form = $(this);
-  //   var action = $form.attr("action");
-  //   var params = $form.serialize();
+// Review.success = function(json) {
+//   var review = new Review(json);
+//   var reviewHTML = review.renderHTML(); 
+//   $("#new_review").trigger("reset");
+//   $('div.reviews').append(reviewHTML)
  
+//   console.log(review)
 
   
+
+// }
+// Review.error = function (response) {
+//   console.log("Yu broke it?", response)
+// }
+
+
+// Review.ready = (function () {
+//   Review.templateSource = $('#review-template').html();
+//   Review.template = Handlebars.compile(Review.templateSource);
 // })
 
-$(function () {
-  Review.ready()
+// Review.prototype.renderHTML = function(){
+//   return Review.template(this)
+// }
 
-  $('form#new_review').submit(function (e) {
-    e.preventDefault();
-    const $form = $(this);
-    const action = $form.attr('action');
-    const params = $form.serialize();
+// $(function () {
+//   Review.ready()
 
-    $.ajax({
-      url: action,
-      data: params,
-      dataType: "json",
-      method: "POST"
-    })
-      .success(Review.success)
-      .error(Review.error)
+//   $('form#new_review').submit(function (e) {
+//     e.preventDefault();
+//     const $form = $(this);
+//     const action = $form.attr('action');
+//     const params = $form.serialize();
 
-  })
+//     $.ajax({
+//       url: action,
+//       data: params,
+//       dataType: "json",
+//       method: "POST"
+//     })
+//       .success(Review.success)
+//       .error(Review.error)
+
+//   })
   
-})
+// })
