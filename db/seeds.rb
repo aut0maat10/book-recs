00e7c8e8b5b428663ca664a86aa92dd7c88fcf0e
Robin Pahlman <robin.pahlman@gmail.com>
2018-04-07 11:26:14 -0400

    add genre seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+fiction = Genre.create(name: 'Fiction')
+poetry = Genre.create(name: 'Poetry')
+technology = Genre.create(name: 'Technology')
